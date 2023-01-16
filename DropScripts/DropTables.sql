7ec1cf02e7e40788114b5241c2f36a0f67c0fb68
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-16 23:49:45 +0300

    Create DropTables.sql

---
+++ DropScripts/DropTables.sql
@@ -0,0 +1,24 @@
+use BankingSector
+
+drop table Clients_SocialStatuses
+go
+drop table SocialStatuses
+go
+drop table  BankCards
+go
+drop table Accounts
+go
+drop table Clients
+go
+drop table  Cities_Banks
+go
+drop table  Cities
+go
+drop table Banks
+go
+
+
+
+
+
+
