f933d18b19a14ebe5f688facbcc49ed5d9b71bff
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-18 00:02:51 +0300

    Update AddMoneyToSocStatus.sql

--- Procedures/AddMoneyToSocStatus.sql
+++ Procedures/AddMoneyToSocStatus.sql
@@ -6,14 +6,35 @@ GO
 CREATE PROCEDURE AddMoneyToSocStatus
     @SocialStatusId INT
 AS
-UPDATE Accounts
-Set Balance = Balance + 10
-FROM SocialStatuses
-	JOIN Clients_SocialStatuses on SocialStatuses.Id = Clients_SocialStatuses.SocialStatusId
-	JOIN Clients on Clients.Id = Clients_SocialStatuses.ClientId
-	JOIN Accounts on Accounts.ClientId = Clients.Id
-Where SocialStatuses.Id = @SocialStatusId
+BEGIN
+	IF NOT EXISTS ( SELECT 1 FROM SocialStatuses  WHERE Id = @SocialStatusId)
+	BEGIN
+		THROW 50100, 'Social status with the same Id is not exist', 1;
+	END
+	IF NOT EXISTS 
+	( 
+	  SELECT 1 
+	  FROM SocialStatuses
+	  JOIN Clients_SocialStatuses on SocialStatuses.Id = Clients_SocialStatuses.SocialStatusId
+	  JOIN Clients on Clients.Id = Clients_SocialStatuses.ClientId
+	  JOIN Accounts on Accounts.ClientId = Clients.Id
+	  WHERE SocialStatuses.Id = @SocialStatusId
+	)
+	BEGIN
+		THROW 50101, 'The status has no linked accounts', 1;
+	END
+
+	UPDATE Accounts
+	Set Balance = Balance + 10
+	FROM SocialStatuses
+		JOIN Clients_SocialStatuses on SocialStatuses.Id = Clients_SocialStatuses.SocialStatusId
+		JOIN Clients on Clients.Id = Clients_SocialStatuses.ClientId
+		JOIN Accounts on Accounts.ClientId = Clients.Id
+	Where SocialStatuses.Id = @SocialStatusId
+END;
 Go
 
-EXEC AddMoneyToSocStatus 1501;
+--Insert into SocialStatuses(Name) values ('test')
+
+EXEC AddMoneyToSocStatus 2001;
 Go
